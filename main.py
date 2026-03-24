71aecb775a970c7d2a89586b154537bcd60b2f9d
code-908 <ospanovjamshid65@gmail.com>
2026-03-24 17:47:18 +0500

    reer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import asyncio
-from os import name
+import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
@@ -7,10 +7,13 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+
+
 bot = Bot(token=os.getenv('BOT_TOKEN'))
 dp = Dispatcher()
-dp.message(CommandStart())
-async def start(message:Message):
+
+@dp.message(CommandStart())
+async def start(message: Message):
     await message.answer('Hello')
 
 async def main():
